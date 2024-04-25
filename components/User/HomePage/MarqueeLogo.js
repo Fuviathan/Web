b914da7a486e98a6d0def50fd3464b7d7cf08d08
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-04-25 21:35:11 +0700

    fix class -> className

--- components/User/HomePage/MarqueeLogo.js
+++ components/User/HomePage/MarqueeLogo.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function MarqueeLogo() {
   return (
-    <div class="relative flex overflow-x-hidden px-4 mt-8 shadow-lg">
-      <div class="flex py-6 animate-marquee whitespace-nowrap">
+    <div className="relative flex px-4 mt-8 overflow-x-hidden shadow-lg">
+      <div className="flex py-6 animate-marquee whitespace-nowrap">
         <img src='/brand-01.png' class="mx-4 text-4xl" />
         <img src='/brand-02.png' class="mx-4 text-4xl" />
         <img src='/brand-03.png' class="mx-4 text-4xl" />
@@ -13,7 +13,7 @@ export default function MarqueeLogo() {
         <img src='/brand-07.png' class="mx-6 text-4xl" />
       </div>
 
-      <div class="flex absolute top-0 py-6 animate-marquee2 whitespace-nowrap">
+      <div className="absolute top-0 flex py-6 animate-marquee2 whitespace-nowrap">
         <img src='/brand-01.png' class="mx-4 text-4xl" />
         <img src='/brand-02.png' class="mx-4 text-4xl" />
         <img src='/brand-03.png' class="mx-4 text-4xl" />
