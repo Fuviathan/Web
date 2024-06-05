35dcd48205fce2588ba73e0a2064f17bfcb0f4c4
Fu <84614670+Fuviathan@users.noreply.github.com>
2024-06-05 13:42:14 +0700

    fix

--- components/User/HomePage/ItemIntroduce.js
+++ components/User/HomePage/ItemIntroduce.js
@@ -12,22 +12,22 @@ export default function ItemIntroduce() {
       </div>
       <div className='relative flex w-1/4 mx-4 rounded-lg shadow-lg h-6/6'>
         <div className='absolute z-10 w-full h-full px-4 pt-6 mb-2 text-black'>
-          <div className='mb-4 text-2xl font-bold'>Loa âm thanh vòm</div>
-          <div className=''>Từ $699 hoặc $116.58/tháng. trong 12 tháng.*</div>
+          <div className='mb-4 text-2xl font-bold'>Luôn cập nhập xu hướng giày</div>
+          <div className=''></div>
         </div>
         <img src='/shoes1.jpg' className='pt-12 rounded-lg' alt='brand' /> 
       </div>    
       <div className='relative flex w-1/4 mx-4 rounded-lg shadow-lg h-6/6'>
         <div className='absolute z-10 w-full h-full px-4 pt-6 text-black '>
-          <div className='mb-4 text-2xl font-bold'>Độ sáng 600 nit</div>
-          <div className=''>Màn hình 27-inch độ phân giải 5K </div>
+          <div className='mb-4 text-2xl font-bold'>Giao dịch trung gian uy tín an toàn</div>
+          <div className=''></div>
         </div>
         <img src='/shoes2.jpg' className='pt-12 rounded-lg' alt='brand' /> 
       </div>   
       <div className='relative flex w-1/4 mx-4 rounded-lg shadow-lg h-6/6'>
         <div className='absolute z-10 w-full h-full px-4 pt-6 text-black'>
-          <div className='mb-4 text-2xl font-bold'>Smartphone 13 Pro</div>
-          <div className=''>Đã có màu xanh. Từ $999.00 hoặc $41.62/tháng. trong 24 tháng. </div>
+          <div className='mb-4 text-2xl font-bold'>Sản phẩm chính hãng</div>
+          <div className=''></div>
         </div>
         <img src='/shoes3.jpg' className='pt-12 rounded-lg' alt='brand' />
       </div>
